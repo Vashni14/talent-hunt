d46b05b87e7b1e9f8402c52bbbd63cfdce6b41f7
Vashni14 <vashninadar123@gmail.com>
2025-07-12 00:17:58 +0530

    deploy try

--- team-mentorship/backend/server.js
+++ team-mentorship/backend/server.js
@@ -148,5 +148,10 @@ io.on("connection", (socket) => {
 // Attach io to app for use in routes
 app.set("io", io);
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use(express.static(path.join(__dirname, '../dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist', 'index.html'));
+});
 const PORT = process.env.PORT || 5000;
 httpServer.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
